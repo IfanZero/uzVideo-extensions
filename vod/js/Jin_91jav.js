1c4026384f6f92d5874f05b0f15bfd1754004055
Ifan <linyangfan940403@126.com>
2025-09-03 16:45:29 +0800

    Update Jin_91jav.js
    category title changed h4->h3

--- vod/js/Jin_91jav.js
+++ vod/js/Jin_91jav.js
@@ -59,7 +59,7 @@ async function getClassList(args) {
             let allClass = $('.pb-3.pb-e-lg-40 .col-6.col-sm-4.col-lg-3')
             let list = []
             allClass.each((index, e) => {
-                const name = $(e).find('h4').text()
+                const name = $(e).find('h3').text()
                 const url = $(e).find('a').attr('href')
                 const isIgnore = isIgnoreClassName(name)
                 if (isIgnore) return
